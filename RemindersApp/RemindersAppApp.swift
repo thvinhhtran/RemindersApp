1160fdf7dcda535dc3f907a6183a64cf1b2062f9
C3 <thvinhhtran29@gmail.com>
2024-10-30 19:54:39 +0700

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Vinh on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
