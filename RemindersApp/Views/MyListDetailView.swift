234eb79bd28066336f4fb4a432914d6fe7d8e853
C3 <thvinhhtran29@gmail.com>
2024-11-05 01:22:26 +0700

    feat: "complete part 6"

---
+++ RemindersApp/Views/MyListDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MyListDetailView.swift
+//  RemindersApp
+//
+//  Created by Vinh on 5/11/24.
+//
+
+import SwiftUI
+
+struct MyListDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyListDetailView()
+}
