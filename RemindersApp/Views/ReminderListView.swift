1f7dee667178cb62c5fe40ce164c1c652aee02f9
C3 <thvinhhtran29@gmail.com>
2024-11-06 20:29:27 +0700

    feat:" display detail view"

---
+++ RemindersApp/Views/ReminderListView.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderListView.swift
+//  RemindersApp
+//
+//  Created by Vinh on 6/11/24.
+//
+
+import SwiftUI
+
+struct ReminderListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderListView()
+}
